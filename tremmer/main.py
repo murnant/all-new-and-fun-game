bd6b0d3bd09736d66f1f19134be8b95a3278f894
Edward Mitchell <edwardinchina@outlook.com>
2021-04-03 20:13:08 -0400

    Update main.py
    fix bugs

--- tremmer/main.py
+++ tremmer/main.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 # Start the game
 pygame.init()
@@ -27,7 +28,8 @@ cam = Camera(0,0)
 size = 10
 player = pygame.Rect( (game_width - size) / 2, (game_height-size) /2, size, size)
 snake = pygame.Rect(0, 0, 150, 150)
-snake_pic = pygame.image.load("Tremor Scales.png")
+print(os.getcwd())
+snake_pic = pygame.image.load("tremmer\Tremor Scales.png")
 snake_pic_small = pygame.transform.scale(snake_pic, (150,150))
 snake_pic_small.set_colorkey((255,255,255))
 
@@ -46,7 +48,7 @@ while running:
             running = False
     screen.fill((102, 51, 0))
     screen.blit(snake_pic_small, (snake.x, snake.y))
-    screen.rect(player,(255,255,255))
+    pygame.draw.rect(screen,(255,255,255),player)
 
     
     if pygame.mouse.get_pressed()[0]:
