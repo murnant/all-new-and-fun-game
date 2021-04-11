942a8266b7bbbb21718335f4429ac4afca5468bd
murnant <74027673+murnant@users.noreply.github.com>
2021-04-11 10:01:51 -0500

    update

--- tremmer/main.py
+++ tremmer/main.py
@@ -31,25 +31,65 @@ player = pygame.Rect( (game_width - size) / 2, (game_height-size) /2, size, size
 
 
 class Snake:
-    def __init__(self,x,y):
+    def __init__(self,x,y,follow_part, isHead):
         self.x = x
         self.y = y
+        if isHead:
+            self.speed = 0.2
+        else:
+            self.speed = 0.1
         self.hitbox = pygame.Rect(0, 0, 150, 150)
         self.pic = pygame.image.load("tremmer\Tremor Scales.png")
         self.pic_small = pygame.transform.scale(self.pic, (150,150))
         self.pic_small.set_colorkey((255,255,255))
+        self.follow_part = follow_part
+        self.isHead = isHead
 
     def draw(self):
         cam.blit(self.pic_small , (self.x , self.y))
+
+    def update (self):
+        if self.isHead:
+            follow_x = player_x
+            follow_y = player_y
+        else:
+            follow_x = self.follow_part.x
+            follow_y = self.follow_part.y
+            
+        if self.x > follow_x:
+            self.x += -self.speed
+            
+        if self.x < follow_x:
+            self.x += self.speed
+                
+        if self.y < follow_y:
+            self.y += self.speed
+                
+        if self.y > follow_y:
+            self.y += -self.speed
+
+    
+
+    def distance(self):
+        f_x = self.follow_part.x
+        f_y = self.follow_part.y
+        self.d = math.sqrt( self.x**2 + self.y**2 )
+
+
 
 
 
 snake = []
 
+follow_part = None
 for i in range (0,5):
-    snake.append(Snake(300,300 + i *100))
-
-speed = 0.1
+    new_part = Snake(300,300 + i *100,follow_part, i == 0)
+    snake.append(new_part)
+    follow_part = new_part
+    
+speed = 1
+player_x = player.x
+player_y = player.y
 
 
 
@@ -65,6 +105,8 @@ while running:
     screen.fill((102, 51, 0))
     pygame.draw.rect(screen,(255,255,255),player)
     for s in snake:
+        s.update()
+    for s in snake:
         s.draw()
 
 
@@ -73,19 +115,26 @@ while running:
         mouse_x, mouse_y = pygame.mouse.get_pos()
 
 
-    if game_width / 2 > mouse_x - size /2:
-        cam.x += -speed
-                
-    if game_width / 2 < mouse_x - size /2:
-        cam.x += speed
-                
-    if game_height /2 < mouse_y - size /2:
-        cam.y += speed
-                
-    if game_height /2 > mouse_y - size /2:
-        cam.y += -speed
+        if game_width / 2 > mouse_x - size /2:
+            cam.x += -speed
+            player_x += -speed
+                    
+        if game_width / 2 < mouse_x - size /2:
+            cam.x += speed
+            player_x += speed
+                    
+        if game_height /2 < mouse_y - size /2:
+            cam.y += speed
+            player_y += speed
+                    
+        if game_height /2 > mouse_y - size /2:
+            cam.y += -speed
+            player_y += -speed
 
 
     # Tell pygame to update the screen
     pygame.display.update()
+    print(cam.x)
+    print(player.x)
+    print(snake[0].x)
 
